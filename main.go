e705762e7a6b7f969a03d1b8f4d016cc3b7a5662
José Arthur <jals@ic.ufal.br>
2021-12-23 19:06:44 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/models"
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/handlers"
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/DAO"
+)
+
+func main(){
+  
+}
