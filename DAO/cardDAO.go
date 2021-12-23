e40535cdec2067a0ac8bb0ff73afbeb8670010bf
José Arthur <jals@ic.ufal.br>
2021-12-23 18:37:05 -0300

    Create cardDAO.go

---
+++ DAO/cardDAO.go
@@ -0,0 +1,17 @@
+package DAO
+
+import (
+
+)
+
+func GetCard() {
+	//recebe o id do usuário e retorna algumas informações do cartão
+}
+
+func CreateCard() {
+	//recebe informações do usuário e retorna o número do cartão
+}
+
+func SuspendCard() {
+	//recebe o id do usuário, altera status do cartão para suspenso e retorna true/false
+}
