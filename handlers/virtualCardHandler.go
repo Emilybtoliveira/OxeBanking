048f20de59a1211e7c670d4a355bc694335b4573
José Arthur <jals@ic.ufal.br>
2021-12-23 18:47:56 -0300

    Create virtualCardHandler.go

---
+++ handlers/virtualCardHandler.go
@@ -0,0 +1,17 @@
+package DAO
+
+import (
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/DAO"
+)
+
+func getAllVirtualCardsHandler() {
+	//redireciona para a função GetAllVirtualCard() em DAO/virtualCardDAO.go
+}
+
+func createVirtualCardHandler()() {
+	//redireciona para a função CreateVirtualCard() em DAO/virtualCardDAO.go
+}
+
+func removeVirtualCardByIDHandler() {
+	//redireciona para a função RemoveVirtualCard() em DAO/virtualCardDAO.go
+}
