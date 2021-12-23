9394e06c7e4121c84ea842d8f0c58f1c43accd20
José Arthur <jals@ic.ufal.br>
2021-12-23 18:40:01 -0300

    Create virtualCardDAO.go

---
+++ DAO/virtualCardDAO.go
@@ -0,0 +1,17 @@
+package DAO
+
+import (
+
+)
+
+func GetAllVirtualCards() {
+	//recebe o id do usuário e retorna os números dos cartões virtuais existentes
+}
+
+func CreateVirtualCard() {
+	//recebe o id do usuário e retorna as informações geradas do cartão virtual
+}
+
+func RemoveVirtualCardByID() {
+	//recebe o id do usuário e o número do cartão virtual a ser removido, retornando true/false
+}
