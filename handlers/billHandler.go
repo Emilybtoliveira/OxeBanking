77d0bd4ec82da9bca5769e5ee78b8b4ba5e92beb
José Arthur <jals@ic.ufal.br>
2021-12-23 18:49:30 -0300

    Create billHandler.go

---
+++ handlers/billHandler.go
@@ -0,0 +1,21 @@
+package DAO
+
+import (
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/DAO"
+)
+
+func getBillHandler() {
+	//redireciona para a função getBill() em DAO/billDAO.go
+}
+
+func getCreditLimitHandler() {
+	//redireciona para a função getCreditLimit() em DAO/billDAO.go
+}
+
+func adjustCreditLimitHandler() {
+	//redireciona para a função adjustCreditLimit() em DAO/billDAO.go
+}
+
+func deductDebtHandler() {
+	//redireciona para a função deductDebt() em DAO/billDAO.go
+}
