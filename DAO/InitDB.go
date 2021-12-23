7641b465e8e43a535b3d45c511575607ba494e49
José Arthur <jals@ic.ufal.br>
2021-12-23 18:33:12 -0300

    Create InitDB()

---
+++ DAO/InitDB.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  //espaço dos imports
+)
+
+func InitDB(){
+	//responsável por iniciar conexão com o banco de dados
+}
+
+func CloseDB(){
+	//responsável por fechar conexão com o banco de dados
+}
