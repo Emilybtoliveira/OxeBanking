9f880c5fd9d7100cbcbf86d05479571c39411270
José Arthur <jals@ic.ufal.br>
2021-12-23 18:46:04 -0300

    Create cardHandler.go

---
+++ handlers/cardHandler.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+  "https://github.com/Emilybtoliveira/OxeBanking/tree/main/DAO"
+)
+
+func getCardHandler() {
+	//redireciona para a função GetCard() em DAO/cardDAO.go
+}
+
+func createCardHandler() {
+	//redireciona para a função CreateCard() em DAO/cardDAO.go
+}
+
+func suspendCardHandler() {
+	//redireciona para a função SuspendCard() em DAO/cardDAO.go
+}
